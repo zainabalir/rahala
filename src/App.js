791dd9b67ca2d6d8b28572a37461282d4b22df56
Zainabalir <Zainabalir465@gmail.com>
2024-10-27 02:43:40 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './compones/Hom';
+import Comp1Bookpage from './comp1booking/Comp1Bookpage';
+import Comp2Bookpage from './comp2booking/Comp2booking';
+import Comp3bookpage from './comp3book/Comp3booking';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />  
+        <Route path="/new-page" element={<Comp1Bookpage/>} /> 
+        <Route path="/new-page-2" element={<Comp2Bookpage/>} /> 
+        <Route path="/new-page-3" element={<Comp3bookpage/>} /> 
+        
+
+
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
+
